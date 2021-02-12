55382c07265c94de5f1278be84debc2096096ebc
Ryan Palmer <ryan@compositional-it.com>
2021-02-12 17:12:05 +0000

    Animation, duration, follow cursor, arrow

--- fable/Feliz.Tippy/Feliz.Tippy.fs
+++ fable/Feliz.Tippy/Feliz.Tippy.fs
@@ -2,10 +2,24 @@
 
 open Fable.Core.JsInterop
 open Fable.Core
+open System
 
 let tippy : obj = importDefault "@tippyjs/react"
+let followCursorPlugin : obj = import "followCursor" "tippy.js"
 
 importAll "tippy.js/dist/tippy.css"
+importAll "tippy.js/animations/scale.css"
+importAll "tippy.js/animations/scale-subtle.css"
+importAll "tippy.js/animations/scale-extreme.css"
+importAll "tippy.js/animations/perspective.css"
+importAll "tippy.js/animations/perspective-subtle.css"
+importAll "tippy.js/animations/perspective-extreme.css"
+importAll "tippy.js/animations/shift-away.css"
+importAll "tippy.js/animations/shift-away-subtle.css"
+importAll "tippy.js/animations/shift-away-extreme.css"
+importAll "tippy.js/animations/shift-toward.css"
+importAll "tippy.js/animations/shift-toward-subtle.css"
+importAll "tippy.js/animations/shift-toward-extreme.css"
 
 let splitChildProps props = 
     let (children, props) = 
@@ -53,7 +67,36 @@ type Placement =
         | AutoStart -> "auto-start"
         | AutoEnd -> "auto-end"
 
-type Milliseconds = Milliseconds of int
+type Milliseconds = 
+    | Milliseconds of int
+    member this.Value =
+        match this with
+        | Milliseconds i -> i
+
+type AnimationVariation =
+    | Normal
+    | Subtle
+    | Extreme
+    member this.Value =
+        match this with
+        | Normal -> String.Empty
+        | Subtle -> "-subtle"
+        | Extreme ->  "-extreme"
+
+type Animation = 
+    | Fade
+    | Scale of AnimationVariation
+    | Perspective of AnimationVariation
+    | ShiftAway of AnimationVariation
+    | ShiftToward of AnimationVariation
+
+    member this.Value =
+        match this with
+        | Fade -> "fade"
+        | Scale v ->  "scale" + v.Value
+        | Perspective v -> "perspective" + v.Value
+        | ShiftAway v -> "shift-away" + v.Value 
+        | ShiftToward v -> "shift-toward" + v.Value
 
 [<Erase>]
 type Tippy =
@@ -61,15 +104,59 @@ type Tippy =
     static member inline content (reactElement:ReactElement) =
         prop.custom("content", reactElement)
 
+    static member inline content (content : string) =
+        prop.custom("content", content)
+
     static member inline placement (position : Placement) =
         prop.custom("placement", position.Value)
 
     static member inline delay (Milliseconds delay) =
         prop.custom("delay", delay)
 
+    // Doesn't work - https://github.com/atomiks/tippyjs-react/issues/94
+    //static member inline allowHTML (allow : bool) =
+    //    prop.custom("allowHTML", allow)
+
+    // Needs plugin - https://atomiks.github.io/tippyjs/v6/all-props/#animatefill
+    //static member inline animateFill =
+    //    prop.custom("animateFill", true)
+
+    /// Requires CSS imports - currently done by default in the binding, may move to Client responsibility.
+    static member inline animation  (animation : Animation) =
+        prop.custom("animation", animation.Value)
+
+    // Not sure how to make this work - https://atomiks.github.io/tippyjs/v6/animations/#inertia
+    //static member inline inertia  =
+    //    prop.custom("inertia", true)
+
+    // Not working, needs a DOM ref I think - https://atomiks.github.io/tippyjs/v6/all-props/#appendto
+    //static member inline appendTo (reactElement : ReactElement) =
+    //    prop.custom("appendTo", reactElement)
+
+    static member inline arrow (enabled : bool) =
+        prop.custom("arrow", enabled)
+
+    /// A string is parsed as .innerHTML. Don't pass unknown user data to this prop.
+    static member inline arrow (svg : string) =
+        prop.custom("arrow", svg)
+
+    static member inline duration (?show : Milliseconds, ?hide : Milliseconds) =
+        prop.custom(
+            "duration", 
+            [| show |> Option.map (fun m -> m.Value)
+               hide |> Option.map (fun m -> m.Value) |])
+
+    /// Requires you to pass "followCursorPlugin" to Tippy.plugins
+    static member inline followCursor (follow : bool) =
+        prop.custom("followCursor", follow)
+
+    static member inline plugins (plugins : obj[]) =
+        prop.custom("plugins", plugins)
+
+    
+
+
     static member inline create (props : IReactProperty seq) = 
-
         let elements = splitChildProps props
-
         Interop.reactApi.createElement(tippy, createObj !!elements.Props, !!elements.Children)
 
--- src/SampleApp/Index.fs
+++ src/SampleApp/Index.fs
@@ -2,8 +2,14 @@ module Index
 
 open Elmish
 
+open Fable.Core.JsInterop
+
+open Fable.React
+open Fable.React.Props
+
 open Feliz.Tippy
 open Feliz
+
 
 type Model =
     { Hello: string }
@@ -21,9 +27,6 @@ let update msg model =
     | GotHello hello ->
         { model with Hello = hello }, Cmd.none
 
-open Fable.React
-open Fable.React.Props
-
 let view model dispatch =
     div [ Style [ TextAlign TextAlignOptions.Center; Padding 40 ] ] [
         div [] [
@@ -31,7 +34,13 @@ let view model dispatch =
             Tippy.create [
                 Tippy.content (em [] [ str "hello world" ])
                 Tippy.placement Auto
-                Tippy.delay (Milliseconds 1000)
+                //Tippy.arrow false
+                //Tippy.arrow "<svg width=\"100\" height=\"100\"><circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"green\" stroke-width=\"4\" fill=\"yellow\" /></svg>"
+                Tippy.delay (Milliseconds 500)
+                Tippy.animation (Animation.Perspective Extreme)
+                Tippy.duration (show = Milliseconds 1000, hide = (Milliseconds 1000))
+                Tippy.plugins [| followCursorPlugin |]
+                Tippy.followCursor true
                 prop.children [h1 [] [ str "feliz_tippy" ]]
             ]
         ]
