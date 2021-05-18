8c3465f7bb536322cf5447066d03f64c62b51551
Akash Mair <akash@compositional-it.com>
2021-05-18 16:13:46 +0100

    added wrapper name as title and updated publish script with repo

--- docs/publish.js
+++ docs/publish.js
@@ -2,7 +2,7 @@ var ghPages = require("gh-pages");
 
 
 // Update with wrapper repo
-var packageUrl = "https://github.com/CompositionalIT/feliz-________.git";
+var packageUrl = "https://github.com/CompositionalIT/feliz-tippy.git";
 
 console.log("Publishing to ", packageUrl);
 
